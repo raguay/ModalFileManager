14de086dfafbffa8e126649e8d66337a77e2586e
Richard Guay <raguay@customct.com>
2023-03-11 11:12:54 +0700

    Added showing the permissions for files/directories in the ExtraPanel and completely fixed the jumping to lower directories based on searching history and files (hopefully)

---
+++ frontend/src/components/ModeLine.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { beforeUpdate } from "svelte";
+
+  export let entry = null;
+  let modeString = "";
+
+  beforeUpdate(() => {
+    modeString = getRWX(entry.mode >> 6) + "  ";
+    modeString += getRWX(entry.mode >> 3) + "  ";
+    modeString += getRWX(entry.mode);
+  });
+
+  function getRWX(bits) {
+    let result = bits & 4 ? "r" : "-";
+    result += bits & 2 ? "w" : "-";
+    result += bits & 1 ? "x" : "-";
+    return result;
+  }
+</script>
+
+<div id="main">
+  <span>{modeString}</span>
+</div>
+
+<style>
+  #main {
+    display: flex;
+    flex-direction: row;
+  }
+
+  #main span {
+    margin-top: 5px;
+    margin-left: 10px;
+  }
+</style>
